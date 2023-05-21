dee29f1797c20a9bc2b48dd6253aa48e921487bc
rando <alex.nighthammer@yandex.ru>
2023-05-21 14:01:50 +0800

    the server's url fix

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "catstronauts-server-production-058d.up.railway.app", // change to YOUR own production server
+  uri: "https://catstronauts-server-production-058d.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
